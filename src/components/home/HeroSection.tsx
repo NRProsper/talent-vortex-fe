bbfbed941a797eeaa93bb6cf1e9a8059abcceb80
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-01-25 15:05:00 +0200

    responsive done

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { Button } from '../ui/button';
-import Image from 'next/image';
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <section className='flex mx-28 my-9 gap-10 items-center justify-between'>
-      <div className='w-[40%]'>
-        <h1 className='text-4xl font-bold text-primary'>Build Work Experience through Skills Challenges Program</h1>
-        <p className='text-xl text-muted-foreground my-9'>Enhance your Employability and Accelerate your Career Growth by working on Hands-on projects & hackathons from various businesses & organizations.</p>
-        <Button size={"xl"}>Get Started</Button>
+    <section className="mx-4 sm:mx-8 md:mx-16 lg:mx-28 my-9 flex flex-col-reverse lg:flex-row lg:gap-10 lg:items-center lg:justify-between">
+      <div className="w-full lg:w-[40%] mb-8 lg:mb-0">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-primary">
+          Build Work Experience through Skills Challenges Program
+        </h1>
+        <p className="text-base sm:text-lg md:text-xl text-muted-foreground my-4 lg:my-9">
+          Enhance your Employability and Accelerate your Career Growth by working on Hands-on projects & hackathons from
+          various businesses & organizations.
+        </p>
+        <Button size={"xl"} className="w-full sm:w-auto">
+          Get Started
+        </Button>
       </div>
-      <div className='flex my-10 gap-5'>
+      <div className="flex flex-col sm:flex-row gap-5 justify-center items-center">
         <Image
           src="/images/hero-img-1.png"
           alt="HeroImage"
           width={300}
           height={300}
+          className="hidden sm:block"
         />
-
         <Image
           src="/images/hero-img-2.png"
           alt="HeroImage"
@@ -28,3 +35,4 @@ export default function HeroSection() {
     </section>
   );
 }
+
