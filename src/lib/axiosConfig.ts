695780fe8b2ae66f5fa2bb7e25fd692ec3a7802a
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-02-09 18:31:06 +0200

    dockerzation done

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -3,9 +3,9 @@ import { getToken, removeToken, isTokenExpired } from "./tokenStorage";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-if (!API_BASE_URL) {
-  throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
-}
+// if (!API_BASE_URL) {
+//   throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+// }
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
